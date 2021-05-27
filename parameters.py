6a060be2174553b1ca312c295ee80f23fd191910
rtitiu <radu.titiu@gmail.com>
2021-05-27 18:54:13 +0300

    updated parameters

--- parameters.py
+++ parameters.py
@@ -2,9 +2,9 @@ from math import log2
 
 # sizes of databases of server and client
 # size of intersection should be less than size of client's database
-server_size = 2 ** 16
-client_size = 5
-intersection_size = 2
+server_size = 2 ** 20
+client_size = 4000
+intersection_size = 3500
 
 # seeds used by both the Server and the Client for the Murmur hash functions
 hash_seeds = [123456789, 10111213141516, 17181920212223]
@@ -24,13 +24,13 @@ number_of_hashes = 3
 sigma_max = int(log2(plain_modulus)) + output_bits - (int(log2(number_of_hashes)) + 1) 
 
 # B = [68, 176, 536, 1832, 6727] for log(server_size) = [16, 18, 20, 22, 24]
-bin_capacity = 68
+bin_capacity = 536
 
 # partitioning parameter
-alpha = 8
+alpha = 16
 
 # windowing parameter
-ell = 1
+ell = 2
 
 f = open("our_results", 'a')
 f.write("|C|="+str(client_size))
